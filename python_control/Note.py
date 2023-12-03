7b0548750cce84b7d30ff8242b1a45acb8785c8c
{RitsukiNakahara} <{n2110453@edu.cc.uec.ac.jp}>
2023-12-03 21:17:42 +0900

    1次遅れ系

--- python_control/Note.py
+++ python_control/Note.py
@@ -11,4 +11,25 @@ s123=series(s12,s3)
 s=feedback(s123,s4)
 print("S=",s)
 
+#%%
+def plot_set(fig_ax,*args):
+    fig_ax.set_xlabel(args[0])
+    fig_ax.set_ylabel(args[1])
+    fig_ax.grid(ls=":")
+    if len(args)==3:
+        fig_ax.legend(loc=args[2])
+
+# %%
+from control.matlab import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+T,K=0.5,1
+P=tf([0,K],[T,1])
+y,t=step(P,np.arange(0,5,0.01))
+
+fig,ax=plt.subplots()
+ax.plot(t,y)
+plot_set(ax,"t","y")
+
 # %%
