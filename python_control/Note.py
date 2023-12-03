c1480ee481c6fef3192e4fb71f0bd38223def7df
{RitsukiNakahara} <{n2110453@edu.cc.uec.ac.jp}>
2023-12-03 16:45:10 +0900

    ブッロク線図

---
+++ python_control/Note.py
@@ -0,0 +1,13 @@
+#%%
+from control import tf,ss,series,feedback
+
+s1=tf([0,1],[1,1])
+s2=tf([0,1],[1,2])
+s3=tf([3,1],[1,0])
+s4=tf([2,0],[0,1])
+
+s12=feedback(s1,s2)
+s123=series(s12,s3)
+s=feedback(s123,s4)
+print(s)
+# %%
