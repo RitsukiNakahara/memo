defc79bbc70a63475438699358533103c3881b33
{RitsukiNakahara} <{n2110453@edu.cc.uec.ac.jp}>
2023-12-08 21:24:57 +0900

    ボード線図(2次遅れ系)

--- python_control/Note.py
+++ python_control/Note.py
@@ -204,7 +204,7 @@ def bodeplot_set(fig_ax,*args):
     fig_ax[0].set_ylabel("Gain [dB]")
     fig_ax[1].grid(which="both",ls=":")
     fig_ax[1].set_xlabel("$\omega$ [rad/s]")
-    fig_ax[0].set_ylabel("Phase [deg]")
+    fig_ax[1].set_ylabel("Phase [deg]")
 
     if len(args)>0:
         fig_ax[1].legend(loc=args[0])
@@ -221,6 +221,21 @@ for i in range(len(T)):
     P=tf([0,K],[T[i],1])
     gain,phase,w=bode(P,logspace(-2,2),plot=False)
     pltargs={"ls":next(LS),"label":"T="+str(T[i])}
+    ax[0].semilogx(w,20*np.log10(gain),**pltargs)
+    ax[1].semilogx(w,phase*180/np.pi,**pltargs)
+
+bodeplot_set(ax,3,3)
+
+# %%　ボード線図(2次遅れ系)
+zeta=[1,0.7,0.4]
+omega_n=1
+
+LS=linestyle_generator()
+fig,ax=plt.subplots(2,1)
+for i in range(len(zeta)):
+    P=tf([0,omega_n**2],[1,2*zeta[i]*omega_n,omega_n**2])
+    gain,phase,w=bode(P,logspace(-2,2),plot=False)
+    pltargs={"ls":next(LS),"label":"$\zeta$="+str(zeta[i])}
     ax[0].semilogx(w,20*np.log10(gain),**pltargs)
     ax[1].semilogx(w,phase*180/np.pi,**pltargs)
 
