f6b363068d2a882620735936dda5293846ecfa3a
{RitsukiNakahara} <{n2110453@edu.cc.uec.ac.jp}>
2023-12-08 21:16:46 +0900

    ボード線図

--- python_control/Note.py
+++ python_control/Note.py
@@ -198,4 +198,32 @@ plot_set(ax,"$x_1$","x_2")
 
 print(s)
 
+#%%  ボード線図
+def bodeplot_set(fig_ax,*args):
+    fig_ax[0].grid(which="both",ls=":")
+    fig_ax[0].set_ylabel("Gain [dB]")
+    fig_ax[1].grid(which="both",ls=":")
+    fig_ax[1].set_xlabel("$\omega$ [rad/s]")
+    fig_ax[0].set_ylabel("Phase [deg]")
+
+    if len(args)>0:
+        fig_ax[1].legend(loc=args[0])
+    if len(args)>1:
+        fig_ax[0].legend(loc=args[1])
+
+# %%　ボード線図
+K=1
+T=[1,0.5,0.1]
+
+LS=linestyle_generator()
+fig,ax=plt.subplots(2,1)
+for i in range(len(T)):
+    P=tf([0,K],[T[i],1])
+    gain,phase,w=bode(P,logspace(-2,2),plot=False)
+    pltargs={"ls":next(LS),"label":"T="+str(T[i])}
+    ax[0].semilogx(w,20*np.log10(gain),**pltargs)
+    ax[1].semilogx(w,phase*180/np.pi,**pltargs)
+
+bodeplot_set(ax,3,3)
+
 # %%
